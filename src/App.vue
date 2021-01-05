52ee6424f166fee5316764d9d9abfb5e0949de1d
Theodore Li <theodoreqili@gmail.com>
2021-01-05 17:19:04 -0600

    Add appbar component with vlf logo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <AppBar/>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
+import AppBar from './components/AppBar';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    AppBar,
+  },
+
+  data: () => ({
+  }),
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="css" scoped>
+@import url(https://fonts.googleapis.com/css?family=Quicksand);
+.header-links {
+  font-size: 24px;
+  font-family: 'Quicksand';
 }
+
+a:link, a:visited {
+  text-decoration: none;
+  color: black;
+}
+
 </style>
---
+++ src/components/AppBar.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-app-bar
+      app
+      color="background"
+      dark
+      flat
+    >
+    <v-img alt="Vivian Likes Fruit" contain height="60" width="0" src="../assets/vlf_logo.jpg"/>
+    <v-spacer/>
+    <div class="header-links" v-for="(appbar_link, i) in appbar_links" :key="i">
+      <v-btn color="black" :href="appbar_link.href" plain>
+        {{ appbar_link.name }}
+      </v-btn>
+    </div>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'AppBar',
+
+  data: () => ({
+    appbar_links: [
+      { name: 'Art', href: 'www.google.com' },
+      { name: 'Blog', href: 'www.google.com' },
+      { name: 'Comics', href: 'www.google.com' },
+      { name: 'Projects', href: 'www.google.com' },
+      { name: 'About', href: 'www.google.com' }
+    ],
+  }),
+};
+</script>
+
+<style lang="css" scoped>
+@import url(https://fonts.googleapis.com/css?family=Quicksand);
+.header-links {
+  font-size: 24px;
+  font-family: 'Quicksand';
+}
+
+a:link, a:visited {
+  text-decoration: none;
+  color: black;
+}
+
+</style>
