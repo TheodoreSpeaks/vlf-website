fcaccd3d0a423e0e553e48f42689941b6aecf67a
Theodore Li <theodoreqili@gmail.com>
2021-01-12 17:10:44 -0600

    Fix bug with id param detection

--- src/views/Comics.vue
+++ src/views/Comics.vue
@@ -1,17 +1,18 @@
 <template>
   <v-app>
     <h1>ID: {{ id }}</h1>
-    <h1>ID: {{ id == null }}</h1>
+    <h1>ID: {{ id }}</h1>
   </v-app>
 </template>
 
 <script>
 export default {
   name: 'Comics',
-  data: () => ({
-    // TODO: change to latest comic #
-    id: this.$route.params.id || -1
-  })
+  computed: {
+    id() {
+      return this.$route.params.id || -1
+    }
+  }
 
 }
 </script>
