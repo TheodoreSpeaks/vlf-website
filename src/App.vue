26102082d27de5852ff62fc65c1a7f1f6e17ced8
Theodore Li <theodoreqili@gmail.com>
2021-01-11 22:45:33 -0600

    Add VueRouter

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,32 @@
 <template>
-  <v-app>
-    <AppBar/>
-    <v-main>
-      <ComicView/>
-    </v-main>
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-import AppBar from './components/AppBar';
-import ComicView from './components/ComicView.vue';
-
-export default {
-  name: 'App',
-
-  components: {
-    AppBar,
-    ComicView,
-  },
-
-  data: () => ({
-  }),
-};
-</script>
-
-<style lang="css">
-@import url(https://fonts.googleapis.com/css?family=Quicksand);
-.header-links {
-  font-size: 24px;
-  font-family: 'Quicksand';
+<style lang="scss">
+#app {
+  // font-family: Avenir, Helvetica, Arial, sans-serif;
+  // -webkit-font-smoothing: antialiased;
+  // -moz-osx-font-smoothing: grayscale;
+  // text-align: center;
+  // color: #2c3e50;
 }
 
+#nav {
+  // padding: 30px;
+
+  // a {
+  //   font-weight: bold;
+  //   color: #2c3e50;
+
+  //   &.router-link-exact-active {
+  //     color: #42b983;
+  //   }
+  // }
+}
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <AppBar/>
+    <ComicView/>
+  </v-app>
+</template>
+
+<script>
+// @ is an alias to /src
+import AppBar from '@/components/AppBar.vue'
+import ComicView from '@/components/ComicView.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    AppBar,
+    ComicView,
+  }
+}
+</script>
